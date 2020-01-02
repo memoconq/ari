0eb28b1c0ef0525ed9656000fae5afb9326b4fe3
Fatih <memoconq@users.noreply.github.com>
2020-01-02 13:16:09 +0300

    v0.1
    Tecrube ve saglik sistemi gelistirildi.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var test;
 var can = 3;
 var seviye = 1;
 var tecrube = 0;
+var saglik = 240;
 
 function topla() {
     oku();
@@ -28,18 +29,34 @@ function carp() {
 function dy() {
     test = beklenen === sonuc;
     if (test) {
-        tecrube = tecrube + 20;
-        if (tecrube > 200) {
+        tecrube = tecrube + 24;
+        if (tecrube > 240) {
             seviye = seviye + 1;
+            tecrube = 0;
+            can = 3
+            saglik = can * 80;
         }
-        document.getElementsByClassName("seviye")[0].style.width = tecrube + "px";
     }
+    else {
+        can = can - 1;
+        saglik = can * 80;
+        if (can === 0) {
+            seviye = seviye - 1;
+            tecrube = 0;
+            can = 3
+            saglik = can * 80;
+        }
+    }
+    document.getElementsByClassName("can")[0].style.width = saglik + "px";
+    document.getElementById("can").innerHTML = can;
+    document.getElementsByClassName("seviye")[0].style.width = tecrube + "px";
+    document.getElementById("seviye").innerHTML = seviye;
     document.getElementById("dy").innerHTML = test;
     console.log("sayi1: " + sayi1);
     console.log("sayi2: " + sayi2);
     console.log("sonuc: " + sonuc);
     console.log("beklenen: " + beklenen);
-    console.log(test);
+    console.log("can: " + can);
 }
 
 function oku() {
